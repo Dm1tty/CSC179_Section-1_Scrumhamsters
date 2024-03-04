7e77aa0670afe6c42a50135b57fd0ee20dbe8cea
MANSOURCSS <Mohamedahmed2@CSUS.EDU>
2024-03-03 19:31:47 -0800

    Update App.js

--- project-healthcare-react-app/App.js
+++ project-healthcare-react-app/App.js
@@ -11,6 +11,7 @@ export default function App() {
         
         <Text>Team members:</Text>
         <Text style={styles.nameIanCharamuga}>Ian Charamuga</Text>
+        <Text style={styles.nameMohamedAhmed}>Mohamed Ahmed</Text>
         <StatusBar style="auto" />
       </View>
       <ExampleComponent/>
@@ -27,5 +28,10 @@ const styles = StyleSheet.create({
   },
   nameIanCharamuga: {
     color: '#FF0000'
+  },
+  nameMohamedAhmed: {
+    color: '#00FF00',
   }
+
+
 });
