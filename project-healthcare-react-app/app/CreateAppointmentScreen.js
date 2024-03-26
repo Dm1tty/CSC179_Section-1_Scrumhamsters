df0c6002b77e05a6e51f250dcca1566791f3aee4
Dzmitry M <ma.dzmitry@gmail.com>
2024-03-26 16:03:16 -0700

    now we can add apts to firebase

--- project-healthcare-react-app/app/CreateAppointmentScreen.js
+++ project-healthcare-react-app/app/CreateAppointmentScreen.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, Text, StyleSheet, Platform } from 'react-native';
+import { View, TextInput, Button, Text, StyleSheet, Platform, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import DatePicker from '@react-native-community/datetimepicker'; // Ensure you have this installed
 import { format } from 'date-fns'; // Assuming you have date-fns installed for date formatting
+
+import { db } from '../firebaseConfig'; // Adjust the import path
+import { collection, addDoc, Timestamp } from 'firebase/firestore';
+
 
 const CreateAppointmentScreen = () => {
   const navigation = useNavigation();
@@ -13,18 +17,41 @@ const CreateAppointmentScreen = () => {
   const [reason, setReason] = useState('');
 
   const onChangeDate = (event, selectedDate) => {
-    setShowDatePicker(Platform.OS === 'ios'); // For iOS, you might want to keep the picker open
+    setShowDatePicker(Platform.OS === 'ios'); // For iOS, to keep the picker open
     const currentDate = selectedDate || date;
     setDate(currentDate);
   };
 
   const dateString = format(date, "PPP"); // Format the date into a readable string
 
-  // Placeholder for function to save appointment
-  const saveAppointment = () => {
-    console.log({ doctor, patient, date, reason });
-    navigation.goBack(); // Navigate back to the previous screen
+  const saveAppointment = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "appointments"), {
+        doctor,
+        patient,
+        date,
+        dateScheduled: Timestamp.fromDate(new Date(date)), // Convert JavaScript Date to Firestore Timestamp
+        reason,
+      });
+      Alert.alert(
+        "Success", // Alert Title
+        "Appointment saved successfully!", // Alert Message
+        [
+          { text: "OK", onPress: () => navigation.goBack() } // Navigate back after pressing OK
+        ]
+      );
+    } catch (error) {
+      console.error("Error saving appointment: ", error);
+      Alert.alert(
+        "Error", // Alert Title
+        "There was a problem saving the appointment. Please try again.", // Alert Message
+        [
+          { text: "OK" } // Button to dismiss the alert
+        ]
+      );
+    }
   };
+  
 
   return (
     <View style={styles.container}>
