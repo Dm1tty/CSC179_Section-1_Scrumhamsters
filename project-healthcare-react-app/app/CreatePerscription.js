2b41f76fbd5825d10b6b16bc1ef243c2c2b46903
Julian Martinez <julianmartinez2@csus.edu>
2024-04-10 16:06:58 -0700

    Create perscription rough draft
    added text fields and necessary styling for the create prescroption page

--- project-healthcare-react-app/app/CreatePerscription.js
+++ project-healthcare-react-app/app/CreatePerscription.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+/*import React, { useState, useEffect } from 'react';
 import { Alert, ScrollView, Text, TextInput, TouchableOpacity, View, Button, StyleSheet } from 'react-native';
 
 import { db } from '../firebaseConfig'; 
@@ -152,3 +152,192 @@ const CreatePerscription = ({ navigation }) => {
 
 
 export default CreatePerscription;
+*/
+
+
+
+
+import React, { useState } from 'react';
+import { View, TextInput, Button, Text, Switch, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { db } from '../firebaseConfig';
+import { collection, doc, setDoc } from 'firebase/firestore';
+import * as ImagePicker from 'expo-image-picker';
+import { getAuth } from "firebase/auth";
+import { getStorage, ref, uploadBytes } from 'firebase/storage';
+import { getDownloadURL } from 'firebase/storage';
+import { Platform } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+
+export default function CreatePerscription() {
+    const auth = getAuth();
+    const [patientfName, setFirstName] = useState('');
+    const [patientlName, setLastName] = useState('');
+    const [DOB, setDOB] = useState(''); // Added for patient date of birth
+    const [email, setEmail] = useState(auth.currentUser?.email || '');
+    const [gender, setGender] = useState(true); // true for Male, false for Female
+    const [specialistType, setSpecialistType] = useState('');
+    const [image, setImage] = useState(null);
+    const navigation = useNavigation(); // Use the useNavigation hook
+
+    const getPermissionAsync = async () => {
+        if (Platform.OS !== 'web') {
+            const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            if (status !== 'granted') {
+                alert('Sorry, we need camera roll permissions to make this work!');
+                return false;
+            }
+            return true;
+        }
+        return false;
+    };
+
+    const pickImage = async () => {
+        const hasPermission = await getPermissionAsync();
+        if (!hasPermission) return;
+
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+        console.log(result); // Log the full result to inspect it
+
+
+        if (!result.cancelled) {
+            const imageUri = result.assets[0].uri; // Access the uri from the first item in the assets array
+            setImage(imageUri);
+            console.log("Image URI set:", imageUri);
+        }
+        else {
+            console.log("Image picking was cancelled");
+        }
+    };
+
+
+    const uriToBlob = async (uri) => {
+        const response = await fetch(uri);
+        const blob = await response.blob();
+        return blob;
+    };
+
+    // Modify the uploadImage function to also return the download URL
+    const uploadImageAndGetURL = async (imageUri) => {
+        console.log("uploadImageAndGetURL called with URI:", imageUri);
+        console.log("Starting upload...");
+        try {
+            const blob = await uriToBlob(imageUri);
+            const storage = getStorage();
+            const storageRef = ref(storage, `profile_Images/${auth.currentUser.uid}`);
+
+
+            await uploadBytes(storageRef, blob); // Ensure this promise resolves
+            console.log('Image uploaded successfully');
+            const downloadURL = await getDownloadURL(storageRef);
+            console.log('Obtained download URL:', downloadURL);
+            return downloadURL;
+        } catch (error) {
+            console.error("Failed to upload image and get URL:", error);
+            throw error; // Rethrow error to catch it outside
+        }
+    };
+
+    const handleSaveProfile = async () => {
+        console.log("handleSaveProfile called");
+        let imageUrl = null;
+        if (image) {
+            try {
+                imageUrl = await uploadImageAndGetURL(image);
+            } catch (error) {
+                alert(`Failed to upload image: ${error.message}`);
+                return; // Exit the function if image upload fails
+            }
+        }
+
+        // Construct the 'name' field using firstName and the first letter of lastName
+        const name = `${firstName} ${lastName.charAt(0)}.`;
+
+        const perscriptionInfo = {
+            patientfName,
+            patientlName,
+            DOB,
+            email,
+            gender: gender ? 'Male' : 'Female',
+            specialistType,
+            image: imageUrl, // Use the uploaded image URL or null if no image was selected
+            name, // Add the constructed 'name' to the perscriptionInfo object
+        };
+
+        try {
+            await setDoc(doc(db, "doctors", auth.currentUser.uid), perscriptionInfo);
+            alert('Profile saved successfully');
+            navigation.reset({
+                index: 0,
+                routes: [{ name: 'Profile' }],
+              });
+
+        } catch (error) {
+            alert(`Error saving profile: ${error.message}`);
+        }
+    };
+
+
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity onPress={pickImage}>
+                {image ? <Image source={{ uri: image }} style={{ width: 200, height: 200 }} /> : <View style={styles.placeholder}><Text>Upload Image</Text></View>}
+            </TouchableOpacity>
+            <TextInput put placeholder="Patient first name" value={patientfName} onChangeText={setFirstName} style={styles.input} />
+            <TextInput placeholder="Patient last Name" value={patientlName} onChangeText={setLastName} style={styles.input} />
+            <TextInput placeholder="Email" value={email} onChangeText={setEmail} style={styles.input} />
+            <View style={styles.switchContainer}>
+                <Text>{gender ? 'Male' : 'Female'}</Text>
+                <Switch value={gender} onValueChange={setGender} />
+            </View>
+            <Picker selectedValue={specialistType} onValueChange={(itemValue, itemIndex) => setSpecialistType(itemValue)} style={styles.picker}>
+                <Picker.Item label="PA" value="PA" />
+                <Picker.Item label="Doctor" value="Doctor" />
+                <Picker.Item label="Nurse" value="Nurse" />
+            </Picker>
+            <Button title="Save Profile" onPress={handleSaveProfile} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#1EB6B9',
+        padding: 20,
+    },
+    input: {
+        width: 300,
+        padding: 10,
+        margin: 10,
+        backgroundColor: 'white',
+        borderRadius: 5,
+    },
+    placeholder: {
+        width: 200,
+        height: 200,
+        backgroundColor: '#ddd',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 10,
+    },
+    switchContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        margin: 10,
+    },
+    picker: {
+        width: 300,
+        backgroundColor: 'white',
+        borderRadius: 5,
+    },
+});
---
+++ project-healthcare-react-app/app/splash.js
@@ -0,0 +1,37 @@
+
+/*      <Image source={require('splash.png')} style={styles.logo} />
+*/
+import React from 'react';
+import { StatusBar, StyleSheet, Text, View, Image } from 'react-native';
+
+const SplashScreen = () => {
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#4db6ac" barStyle="light-content" />
+      <Text style={styles.title}>Patient Diary</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#4db6ac',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    // You'll need to adjust the size to match the screenshot
+    width: 100,
+    height: 100,
+    // Add any additional styling to match the screenshot, such as color if the image is transparent
+  },
+  title: {
+    marginTop: 20, // Adjust the spacing as needed
+    fontSize: 24, // Adjust the font size as needed
+    fontWeight: 'bold',
+    color: 'white', // Adjust the color to match the screenshot
+  },
+});
+
+export default SplashScreen;
