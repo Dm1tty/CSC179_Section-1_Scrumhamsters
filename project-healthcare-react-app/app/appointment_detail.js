825061a6f41df44a28694745f073cc21da85aa9b
Dzmitry M <ma.dzmitry@gmail.com>
2024-04-23 19:53:01 -0700

    made buttons clickable

--- project-healthcare-react-app/app/appointment_detail.js
+++ project-healthcare-react-app/app/appointment_detail.js
@@ -1,5 +1,10 @@
-import { useRoute } from '@react-navigation/native';
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { db } from '../firebaseConfig';
+import { getDoc, doc, updateDoc } from 'firebase/firestore';
+
+
+
 import {
   View,
   Text,
@@ -7,45 +12,150 @@ import {
   StyleSheet,
   Dimensions,
   TouchableOpacity,
+  ActivityIndicator,
+  Alert,
 } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 const windowWidth = Dimensions.get("window").width;
 
-const AppointSchreen = () => {
+const AppointmentDetailScreen = () => {
   const route = useRoute();
+  const navigation = useNavigation();
   const { appointmentId } = route.params ?? {};  // Provides an empty object as fallback
+  const [patient, setPatient] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [appointment, setAppointment] = useState(null);
+  const [illnesses, setIllnesses] = useState([]);
+
   console.log("Route params received:", route.params);
+
+
+  useEffect(() => {
+    const fetchAppointmentAndPatient = async () => {
+        setLoading(true);
+        try {
+            const appointmentRef = doc(db, 'appointments', appointmentId);
+            const appointmentSnap = await getDoc(appointmentRef);
+
+            if (appointmentSnap.exists()) {
+                const appData = appointmentSnap.data();
+                appData.date = appData.date ? appData.date.toDate().toDateString() : 'N/A';
+                setAppointment(appData);
+
+                const patientRef = doc(db, 'patients', appData.patient);
+                const patientSnap = await getDoc(patientRef);
+
+                if (patientSnap.exists()) {
+                    const patientData = patientSnap.data();
+                    setPatient(patientData);
+                    
+                    // Access the illnesses array directly from the patient data
+                    if (patientData.illnesses && patientData.illnesses.length > 0) {
+                        setIllnesses(patientData.illnesses);
+                        console.log("Illnesses:", patientData.illnesses);
+                    } else {
+                        setIllnesses([]);
+                        console.log("No illnesses listed in the patient document.");
+                    }
+                } else {
+                    console.log('No such patient!');
+                    setPatient(null);
+                    setIllnesses([]);
+                }
+            } else {
+                console.log('No such appointment!');
+                setAppointment(null);
+            }
+        } catch (error) {
+            console.error("Error fetching data: ", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    fetchAppointmentAndPatient();
+}, [appointmentId]);
+
+
+  // Handler to navigate to a different screen
+  const handleAppointmentCompletion = () => {
+    navigation.navigate('visit_summary'); 
+  };
+
+  const handleAppointmentCancellation = () => {
+    Alert.alert(
+      "Confirm Cancellation",
+      "Are you sure you want to cancel this appointment?",
+      [
+        {
+          text: "Go Back",
+          onPress: () => console.log("Cancellation aborted"),
+          style: "cancel"
+        },
+        { 
+          text: "Confirm", 
+          onPress: () => {
+            console.log("Confirmed cancellation");
+            cancelAppointment();
+          }
+        }
+      ]
+    );
+  };
+  
+  const cancelAppointment = async () => {
+    const appointmentRef = doc(db, 'appointments', appointmentId); 
+  
+    try {
+      await updateDoc(appointmentRef, {
+        status: "Cancelled"
+      });
+      console.log('Appointment successfully cancelled.');
+      
+      // Optionally, navigate back or refresh the data here
+    } catch (error) {
+      console.error("Error updating document: ", error);
+    }
+  };
+
+  if (loading) {
+    return <ActivityIndicator size="large" color="#0000ff" />;
+  }
+  const illnessNames = illnesses.map(illness => illness.name).join(', ');
+  console.log("ilnesses: " + illnessNames);
 
   return (
     <View>
-            <Text>Appointment ID: {appointmentId}</Text>
       <View style={styles.container}>
-        <TouchableOpacity style={styles.backButton}>
-          <Icon name="arrow-left" size={20} color="#FFFFFF" />
-        </TouchableOpacity>
-        <View style={styles.circularImageView}>
-          <Image source={require('../assets/icon.png')} />
-        </View>
+          <Image
+            source={patient?.image ? { uri: patient.image } : require('../assets/icon.png')}
+            style={styles.circularImageView}
+          />
+
         <View style={styles.content}>
-          <Text style={styles.name}>Anupama Gurung</Text>
+          <Text style={styles.name}>{patient.firstName} {patient.lastName}</Text>
           <Text style={styles.age}>32 yrs</Text>
         </View>
       </View>
       <View style={styles.detailesConatainer}>
-        <Text style={styles.dateTimeText}>Wed ,12 May 24 |10:00 PM</Text>
+        <Text style={styles.dateTimeText}>{appointment.date} {appointment.time}</Text>
         <View style={styles.itemContainer}>
           {/* Horizontal items */}
-          <View style={styles.item}>
-            {/* Icon image */}
-            <Image
-              source={require('../assets/mark-icon.png')}
-              style={styles.icon}
-            />
-            {/* Text */}
-            <Text style={styles.itemText}>Mark As{"\n"}Completed</Text>
-          </View>
+          <TouchableOpacity onPress={handleAppointmentCompletion}>
+            <View style={styles.item}>
+              {/* Icon image */}
+              <Image
+                source={require('../assets/mark-icon.png')}
+                style={styles.icon}
+              />
+              {/* Text */}
+              <Text style={styles.itemText}>Mark As{"\n"}Completed</Text>
+            </View>
+          </TouchableOpacity>
 
+
+          <TouchableOpacity onPress={handleAppointmentCancellation}>
           <View style={styles.item}>
             {/* Icon image */}
             <Image
@@ -55,6 +165,7 @@ const AppointSchreen = () => {
             {/* Text */}
             <Text style={styles.itemText}>Cancel{"\n"}Appointment</Text>
           </View>
+          </TouchableOpacity>
 
           <View style={styles.item}>
             {/* Icon image */}
@@ -72,41 +183,37 @@ const AppointSchreen = () => {
         <Text style={styles.tagContainer}>Patient Detailes</Text>
         <View style={styles.keyValueContainer}>
           <Text style={styles.key}>Full Name</Text>
-          <Text style={styles.value}>Anupama Gurung</Text>
+          <Text style={styles.value}>{patient.firstName} {patient.lastName}</Text>
         </View>
         <View style={styles.keyValueContainer}>
-          <Text style={styles.key}>Age:</Text>
-          <Text style={styles.value}>32 Years</Text>
+          <Text style={styles.key}>DOB:</Text>
+          <Text style={styles.value}>{patient.dateOfBirth}</Text>
         </View>
         <View style={styles.keyValueContainer}>
           <Text style={styles.key}>Gender</Text>
-          <Text style={styles.value}>Female</Text>
+          <Text style={styles.value}>{patient.gender}</Text>
         </View>
         <View style={styles.keyValueContainer}>
           <Text style={styles.key}>Phone NO:</Text>
-          <Text style={styles.value}>7500190739</Text>
-        </View>
-        <View style={styles.keyValueContainer}>
-          <Text style={styles.key}>Address:</Text>
-          <Text style={styles.value}>2/4 ST. Tilak Road ,Dehradun</Text>
+          <Text style={styles.value}>{patient.mobilePhone}</Text>
         </View>
         <View style={styles.keyValueContainer}>
           <Text style={styles.key}>Medical History</Text>
-          <Text style={styles.value}>NO</Text>
+          <Text style={styles.value}>{illnessNames}</Text>
         </View>
 
         <Text style={styles.tagContainer}>Prescription</Text>
 
       </View>
       <View style={styles.itemContainer}>
-      <Image
-              source={require('../assets/prescription-icon.png')}
-              style={styles.document}
-            />
         <Image
-              source={require('../assets/prescription-icon.png')}
-              style={styles.document}
-            />
+          source={require('../assets/prescription-icon.png')}
+          style={styles.document}
+        />
+        <Image
+          source={require('../assets/prescription-icon.png')}
+          style={styles.document}
+        />
       </View>
       <TouchableOpacity style={styles.button} >
         <Text style={styles.buttonText}>Save</Text>
@@ -125,19 +232,19 @@ const styles = StyleSheet.create({
   },
   key: {
     fontSize: 14,
-    color:'#888888'
+    color: '#888888'
     // Add any other styles as needed
   },
   value: {
     fontSize: 12,
-    color:'#666666'
+    color: '#666666'
     // Add any other styles as needed
   },
   container: {
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-   
+
     backgroundColor: "#1EB6B9", // Background color for toolbar
     paddingVertical: 10,
     width: windowWidth, // Set width to the width of the device screen
@@ -208,7 +315,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#F2F2F2",
     padding: 10,
     fontWeight: "bold",
-    color:'#666666',
+    color: '#666666',
     marginTop: 10,
   },
   patientDetaileContainer: {
@@ -220,8 +327,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 60,
     borderRadius: 10,
     marginTop: 20,
-    marginHorizontal:20
-  
+    marginHorizontal: 20
+
   },
   buttonText: {
     color: '#fff',
@@ -236,4 +343,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default AppointSchreen;
+export default AppointmentDetailScreen;
